7010cc51f5bfd4434822dca74689028de4ab90bf
miya <rinn.sasami@gmail.com>
2021-04-05 14:13:45 +0900

    テスト練習

---
+++ spec/student_spec.rb
@@ -0,0 +1,23 @@
+RSpec.describe Student do
+  describe ".initalize" do
+    context "インスタンスが生成された時" do
+      it "名前の値が正しいこと" do
+        # テスト内容
+        student = Student.new(name: "田中", age: 17)
+        expect(student.name).to eq "田中"
+      end
+
+      it "年齢の値が正しいこと" do
+        student = Student.new(name: "田中", age: 17)
+        expect(student.age).to eq 17
+      end
+    end
+  end
+
+  describe '.introduce' do
+    it '正しい名前と年齢を含む値が返ること' do
+      student = Student.new(name: '田中', age: 17)
+      expect(student.introduce).to eq '私は田中です。年齢は17歳です。'
+    end
+  end
+end
